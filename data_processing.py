6a44670b7f30d5bcb3c82a7b792bb5303e0945cd
Guilherme Fernandes <aateg@live.com>
2022-05-23 11:50:56 +0200

    WIP feat: pipeline for transforming the data

---
+++ data_processing.py
@@ -0,0 +1,103 @@
+import pathlib
+import numpy as np
+import pandas as pd
+from ecgdetectors import Detectors
+from pyhrv.time_domain import sdnn, sdann, nn50, sdsd, tinn, rmssd
+from utils import load_ecg, csv_export
+
+def rr_peaks_from_ecg_signal(ecg_signal: pd.Series) -> np.array:
+    """Given an ECG signal function gets the RR peaks
+    :param ecg_signal: ECG signal
+    :return: array with peak positions ??? TODO verify that
+    """
+    fs = 300 # Sampling frequency
+    detectors = Detectors(fs)
+
+    r_peaks = detectors.hamilton_detector(ecg_signal) # RR distance detection
+    
+    rr = np.diff(r_peaks) / fs * 1000
+    return rr
+
+
+def normalize(ecg_signal: np.array) -> np.array:
+    """"Normalice de an ECG
+        Introducing a vector of the electrocardigrom
+        and it retuns the vector normalizated
+
+    """
+    avg, dev = ecg_signal.mean(), ecg_signal.std()
+    return  (ecg_signal - avg) / dev
+
+
+def apply_metrics(peaks: np.array) -> pd.DataFrame:
+    """Given the RR peaks array, returns a DataFrame
+    with all features of interess
+    
+    * SDNN: Standard deviation of RR intervals series
+    * SDANN: Standard deviation of the mean of RR intervals in 5-min segments
+    * pNN50: Proportion of adjacent RR intervals differing by more than 50 ms
+    * SDSD: Standard deviation of differences between adjacent RR intervals
+    * TINN: Baseline width of the triangular interpolation of the intervals histogram
+    * r-MSSD: The square root of the mean of the squares of differences between adjacent RR intervals
+    """
+    
+    return np.array([
+        sdnn(peaks)["sdnn"],
+        #sdann(peaks)["sdann"], # warnings.warn("Signal duration too short for SDANN computation.")
+        nn50(peaks)["nn50"],
+        sdsd(peaks)["sdsd"],
+        #tinn(peaks, plot=False)["tinn"], # warnings.warn('CAUTION: The TINN computation is currently providing incorrect results in the most cases due to a 
+        rmssd(peaks)["rmssd"]
+    ])
+
+
+def pipeline(ecg_signal: np.array) -> np.array:
+    """Process an ECG Signal
+    """
+    # normalize data
+    normalized_ecg = normalize(ecg_signal)
+
+    # remove noise
+    #ohne_noise_ecg = remove_noise(normalized_ecg)
+    
+    # TODO remove artifacts
+
+    # extract rr peaks
+    rr_peaks = rr_peaks_from_ecg_signal(normalized_ecg)
+
+    # extract metrics
+    metrics = apply_metrics(rr_peaks)
+
+    return metrics
+
+
+def process_all_ecg() -> pd.DataFrame:
+    """Load and transform data using Pipeline functions
+    :returns: table which each line corresponds to one ECG signal and each column one metric of the signal
+    """
+    features = []
+
+    # getting paths
+    repo_dir = pathlib.Path(__file__).parent
+    training_dir = repo_dir / "training/"
+
+    # applying functions in all files
+    for data in training_dir.iterdir():
+        if data.suffix == ".mat":
+            ecg_signal = load_ecg(data.name)
+            features.append(pipeline(ecg_signal))
+            print(f"Processing: {data.name}...")
+
+    return np.array(features)
+
+def process_one_ecg():
+    ecg_signal = load_ecg("train_ecg_00001.mat")
+
+    data = pipeline(ecg_signal)
+    csv_export(data)
+
+
+if __name__ == "__main__":
+    data = process_all_ecg()
+    csv_export(data)
+    
--- utils.py
+++ utils.py
@@ -2,8 +2,6 @@ import pandas as pd
 import numpy as np
 import pathlib
 from scipy.io import loadmat
-from ecgdetectors import Detectors
-from pyhrv.time_domain import sdnn, sdann, nn50, sdsd, tinn, rmssd
 
 def load_ecg(filename: str) -> pd.Series:
     """Function to load ECG signal as pandas Series
@@ -21,31 +19,6 @@ def load_ecg(filename: str) -> pd.Series:
 
     return ecg
 
-
-def rr_peaks_from_ecg_signal(ecg_signal: pd.Series) -> np.array:
-    """Given an ECG signal function gets the RR peaks
-    :param ecg_signal: ECG signal
-    :return: array with peak positions ??? TODO verify that
-    """
-    fs = 300 # Sampling frequency
-    detectors = Detectors(fs)
-
-    r_peaks = detectors.hamilton_detector(ecg_signal) # RR distance detection
-    
-    rr = np.diff(r_peaks) / fs * 1000
-    return rr
-    
-    
-def normalize(ecg_signal: np.array) -> np.array:
-    """"Normalice de an ECG
-        Introducing a vector of the electrocardigrom
-        and it retuns the vector normalizated
-
-    """
-    avg, dev = ecg_signal.mean(), ecg_signal.std()
-    return  (ecg_signal - avg) / dev
-
-
 def get_target(filenamestem: str):
     repo_dir = pathlib.Path(__file__).parent
     training_dir = repo_dir / "training/"
@@ -53,33 +26,14 @@ def get_target(filenamestem: str):
     return reference_df[reference_df["filename"] == filenamestem]["label"].values[0]
 
 
-def csv_export(data: np.array, path: pathlib.Path, name: str) -> None:
+def csv_export(data: np.array, path: pathlib.Path = None, name: str = None) -> None:
     """Export numpy array as CSV
     :param data: array to be exported
     :param path: path to be exported
     :param name: name of the file
     """
-    new_name = (get_target(name[:-4]) + "_" + name)
-    pd.Series(data).to_csv(path / new_name, index=False)
-    
-    
-def apply_metrics(peaks: np.array) -> pd.DataFrame:
-    """Given the RR peaks array, returns a DataFrame
-    with all features of interess
-    
-    * SDNN: Standard deviation of RR intervals series
-    * SDANN: Standard deviation of the mean of RR intervals in 5-min segments
-    * pNN50: Proportion of adjacent RR intervals differing by more than 50 ms
-    * SDSD: Standard deviation of differences between adjacent RR intervals
-    * TINN: Baseline width of the triangular interpolation of the intervals histogram
-    * r-MSSD: The square root of the mean of the squares of differences between adjacent RR intervals
-    """
-    
-    return pd.DataFrame.from_dict({
-        "SDNN": sdnn(peaks),
-        "SDANN": sdann(peaks),
-        "NN50": sdnn(peaks),
-        "SDSD": sdnn(peaks),
-        "TINN": sdnn(peaks),
-        "r-MSSD": rmssd(peaks)
-    })
+    np.savetxt("features.csv", data, delimiter=",")
+    # new_name = (get_target(name[:-4]) + "_" + name)
+    # pd.Series(data).to_csv(path / new_name, index=False)
+
+
